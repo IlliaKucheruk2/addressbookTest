dc0049021ffd8b65a8ef2d43977b70c22940d25a
Your Name <you@example.com>
2022-07-20 10:28:50 -0400

    setup views

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
+const app = express();
+
 
 dotenv.config({ path:'config.env'})
 const PORT = process.env.PORT||8080
@@ -25,7 +27,7 @@ app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 
 app.get('/',(req,res)=>{
-    res.send("Crud Application");
+    res.render('index');
 })
 
-app.listen(3000, ()=> {console.log(`Server is running pn http://localhost:${PORT}`)});
+app.listen(PORT,()=> {console.log(`Server is running pn http://localhost:${PORT}`)});
